8c029d0d8bf3363ab620e705d7063ea5881ca48f
ee19b048 <ee19b048@smail.iitm.ac.in>
2022-05-29 16:19:32 +0530

    problem #1634 accepted.

--- main.cpp
+++ main.cpp
@@ -17,40 +17,33 @@ int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, res = 0;
-    cin >> n;
-    int a[n];
-    read_array<int>(a, n);
-    int i = 0, j = 0;
-    set<int> s;
-    while(j<n){
-        if(s.empty()){
-            s.insert(a[j]);
-            // cout << a[j] << " inserted" << endl;
-            j += 1;
-            res = max(j-i, res);
-        }
-        else if(s.count(a[j])){
-            while(a[i] != a[j]){
-                s.erase(s.find(a[i]));
-                // cout << a[i] << " removed" << endl;
-                i += 1;
-            }
-            s.erase(s.find(a[i]));
-            // cout << a[i] << " removed" << endl;
-            i += 1;
-            s.insert(a[j]);
-            // cout << a[j] << " inserted" << endl;
-            j += 1;
-            res = max(j-i, res);
-        }
-        else{
-            s.insert(a[j]);
-            // cout << a[j] << " inserted" << endl;
-            j += 1;
-            res = max(res, j-i);
-        }
+    int n, x;
+    cin >> n >> x;
+    int c[n];
+    read_array<int>(c, n);
+    sort(c, c+n);
+    int a[x+1];
+    a[0] = 0;
+    if(x < c[0]){
+        cout << -1 << endl;
     }
-    cout << res << endl;
+    else{
+        for(int i = 0; i<n; i+=1){
+            a[c[i]] = 1;
+        }
+        for(int i = 1; i<=x; i++){
+            int mn = INT_MAX;
+            for(int j = 0; j<n && i-c[j]>=0; j++){
+                mn = min(mn, a[i-c[j]]);
+            }
+            if(mn == INT_MAX){
+                a[i] = INT_MAX;
+            }
+            else{
+                a[i] = mn + 1;
+            }
+        }
+        cout << ((a[x] == INT_MAX) ? -1 : a[x]) << endl;
+    }
 
 }
